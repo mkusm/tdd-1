27ccc7909de0a4b805c38b5897dbb7f15148abf6
Michal Kusmierczyk <kusm@protonmail.ch>
2017-07-17 19:27:02 +0200

    New template - list.html

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -86,7 +86,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis starts a new list by entering a new item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
